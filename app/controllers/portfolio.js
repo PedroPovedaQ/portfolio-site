4d426c15ce780331ee68d58affce11797817747a
Pedro Poveda <>
2019-04-01 02:48:36 -0400

    feature: transitions

--- app/controllers/portfolio.js
+++ app/controllers/portfolio.js
@@ -6,18 +6,21 @@ export default Controller.extend({
     {
       title: "MetaGrid™",
       description: `MetaGrid’s interactive dashboards and data visualizations facilitate rapid investigation and comprehensive understanding of risk.  Through these intuitive visualizations, every stakeholder—from IT, to security and risk teams, to executive management—can quickly understand the extent and impact of cyber incidents within their network in a single unified dashboard.  MetaGrid's powerful drill-down capabilities take you directly to the root cause for immediate remediation. Faster response time dramatically reduces the potential impact on your organization.`,
-      imgURL: "img/metagrid.png"
+      imgURL: "img/metagrid.png",
+      link: "https://www.redlambda.com/metagrid"
     },
     {
       title: "Rumii.net",
       description: "Material Design website featuring mobile-first design.",
-      imgURL: "img/rumii.png"
+      imgURL: "img/rumii.png",
+      link: "https://www.rumii.net/landing"
     },
     {
       title: "Knetik Cloud",
       description:
         "Knetik Cloud's visualization rich dashboard allows you to both manage and analyze the usage of your created container.",
-      imgURL: "img/knetik.png"
+      imgURL: "img/knetik.png",
+      link: "https://apps.knetikcloud.com/"
     }
   ]
 });
---
+++ app/transitions.js
@@ -0,0 +1,48 @@
+export default function() {
+  // Add your transitions here, like:
+  this.transition(
+    this.fromRoute("index"),
+    this.toRoute("education"),
+    this.use("toUp"),
+    this.reverse("toDown")
+  );
+  this.transition(
+    this.fromRoute("index"),
+    this.toRoute("skills"),
+    this.use("toUp"),
+    this.reverse("toDown")
+  );
+  this.transition(
+    this.fromRoute("index"),
+    this.toRoute("portfolio"),
+    this.use("toUp"),
+    this.reverse("toDown")
+  );
+  this.transition(
+    this.fromRoute("index"),
+    this.toRoute("portfolio"),
+    this.use("toUp"),
+    this.reverse("toDown")
+  );
+
+  this.transition(
+    this.fromRoute("skills"),
+    this.toRoute("portfolio"),
+    this.use("toUp"),
+    this.reverse("toDown")
+  );
+
+  this.transition(
+    this.fromRoute("skills"),
+    this.toRoute("education"),
+    this.use("toUp"),
+    this.reverse("toDown")
+  );
+
+  this.transition(
+    this.fromRoute("portfolio"),
+    this.toRoute("education"),
+    this.use("toUp"),
+    this.reverse("toDown")
+  );
+}
